9bf903d2148a93f58d6391c594c0bd666dca8f9d
fyemane <93152842+fyemane@users.noreply.github.com>
2026-03-07 23:00:50 -0800

    import libaries for blueprints\auth\routes.py

--- blueprints/auth/routes.py
+++ blueprints/auth/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
-
-auth_bp = Blueprint("auth", __name__)
-
-from . import routes
+import requests
+from flask import request, jsonify, render_template, session, redirect, url_for
+from firebase_admin import auth
+from firebase import db
+from config import Config
+from . import auth_bp
